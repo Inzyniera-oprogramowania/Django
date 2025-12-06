ad225baebc230038183639105ebfe71ed7d1b5a6
Barry4747 <bartek.rasztar24@gmail.com>
2025-12-06 14:58:59 +0100

    CI fix and linting issues fix

--- pollution_backend/sensors/apps.py
+++ pollution_backend/sensors/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
+
 class SensorsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "pollution_backend.sensors"  
+    name = "pollution_backend.sensors"
