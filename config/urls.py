882235a2183a9bbeabc9242fb456c59a4f647d15
Barry4747 <bartek.rasztar24@gmail.com>
2025-12-07 22:22:47 +0100

    users cleanup

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path("users/", include("pollution_backend.users.urls", namespace="users")),
+    path("users/", include("pollution_backend.users.api.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     # ...
---
+++ pollution_backend/users/api/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
+
+from pollution_backend.users.api.views import UserViewSet
+
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
+
+router.register("users", UserViewSet)
+
+app_name = "users"
+urlpatterns = router.urls
