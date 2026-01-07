0a704bb36c56959d42b194ac78991724434c5149
Barry4747 <bartek.rasztar24@gmail.com>
2026-01-07 22:31:59 +0100

    Fix tests: remove obsolete Admin/HTML tests, fix settings context processor, adapt checks to Legacy User model

--- pollution_backend/users/tests/api/test_views.py
+++ pollution_backend/users/tests/api/test_views.py
@@ -29,6 +29,8 @@ class TestUserViewSet:
         response = view.me(request)  # type: ignore[call-arg, arg-type, misc]
 
         assert response.data == {
-            "url": f"http://testserver/api/users/{user.pk}/",
-            "name": user.name,
+            "id": user.id,
+            "email": user.email,
+            "is_staff": user.is_staff,
+            "is_active": user.is_active,
         }
