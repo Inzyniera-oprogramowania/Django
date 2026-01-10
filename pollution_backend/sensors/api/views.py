493cbd549cf36f8ba5942103a8bdd41ee089b999
Kamil Borusiak <71826693+Kamok1@users.noreply.github.com>
2026-01-10 18:15:40 +0100

    Add all stations endpoint

--- pollution_backend/sensors/api/views.py
+++ pollution_backend/sensors/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.response import Response
 
+from rest_framework.decorators import action
 from pollution_backend.selectors.sensors import get_active_sensors
 from pollution_backend.selectors.sensors import get_active_stations
+from pollution_backend.selectors.sensors import get_all_stations
 from pollution_backend.selectors.sensors import get_norms
 from pollution_backend.selectors.sensors import get_pollutants
 from pollution_backend.sensors.models import MonitoringStation
@@ -11,6 +14,7 @@ from pollution_backend.sensors.models import QualityNorm
 from pollution_backend.sensors.models import Sensor
 
 from .serializers import MonitoringStationDetailSerializer
+from .serializers import MonitoringStationFlatSerializer
 from .serializers import MonitoringStationGeoSerializer
 from .serializers import PollutantSerializer
 from .serializers import QualityNormSerializer
@@ -19,12 +23,19 @@ from .serializers import SensorSerializer
 
 from drf_spectacular.utils import extend_schema
 
+
 class MonitoringStationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = MonitoringStation.objects.all()
 
     @extend_schema(exclude=True)
     def list(self, request, *args, **kwargs):
          return super().list(request, *args, **kwargs)
+
+    @action(detail=False, url_path="all")
+    def all_stations(self, request):
+        queryset = get_all_stations()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def get_queryset(self):
         return get_active_stations()
@@ -36,6 +47,8 @@ class MonitoringStationViewSet(viewsets.ReadOnlyModelViewSet):
     def get_serializer_class(self):
         if self.action == "list":
             return MonitoringStationGeoSerializer
+        if self.action == "all_stations":
+            return MonitoringStationFlatSerializer
         return MonitoringStationDetailSerializer
 
 
@@ -45,6 +58,13 @@ class SensorViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         station_id = self.request.query_params.get("station_id")
+        if station_id:
+            try:
+                station_id = int(station_id)
+            except ValueError:
+                station_id = None
+        else:
+            station_id = None
         return get_active_sensors(station_id=station_id)
 
 
